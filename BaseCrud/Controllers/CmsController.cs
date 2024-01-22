2fb473fccc615b4e9193b63ab99d4fff68020627
imall <imall841110@gmail.com>
2024-01-22 17:54:13 +0800

    新增create頁面

--- BaseCrud/Controllers/CmsController.cs
+++ BaseCrud/Controllers/CmsController.cs
@@ -1,4 +1,5 @@
 ﻿using BaseCrud.Data;
+using BaseCrud.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -28,6 +29,28 @@ namespace BaseCrud.Controllers
             if (id == null) return NotFound();
             var employee = await _context.Employee.FirstOrDefaultAsync(m => m.Id == id);
             if (employee == null) return NotFound();
+            return View(employee);
+        }
+
+
+        /// <summary>
+        /// 建立新資料頁
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create([Bind("Id,Name,Mobile,Email,Department,Title")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
             return View(employee);
         }
     }
